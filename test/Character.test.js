3f81dadc059eb7c5f24c6313fddb29643131de8d
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2023-02-02 15:44:32 -0300

    TDD iteration

--- test/Character.test.js
+++ test/Character.test.js
@@ -1,4 +1,4 @@
-const Game=require('../Game/Game.js')
+const Game=require('../Game/Game')
 test('crear jugador',()=>{
     const game=new Game
     expect(game.crearjugador(1)).toBe('jugador creado');
