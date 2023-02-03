f1d3399b7250bfaf21dfbc24f1f5f2884b4366e1
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2023-02-03 14:28:29 -0300

    TDD iteration

--- test/Character.test.js
+++ test/Character.test.js
@@ -1,23 +1,7 @@
 const Player=require('../Game/Player')
+const Personaje=require('../Game/Player')
 const Game=require('../Game/Game')
-test('crear jugador',()=>{
-    let game= new Game()
-    expect(game.crearjugador(1)).toBe('jugador creado');
-})
-test('Mapa',()=>{
-
-    let game= new Game()
-
-    expect(game.nivel()).toStrictEqual([[],[],[]],
-                                       [[],[],[]],
-                                       [[],[],[]]);
-
-    
-})
-test('move player',()=>{
-
-    let player= new Player
-    expect(game.nivel()).toStrictEqual() 
-
-
+test('crear Personaje nombre',()=>{
+    const personaje= new Personaje
+    expect(personaje.nombre('Juan')).toBe('juan');
 })
