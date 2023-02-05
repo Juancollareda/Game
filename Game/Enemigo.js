077712f784f220ffec3cc7bdf05d32e0686fffa4
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2023-02-05 14:53:32 -0300

    TDD iteration

--- Game/Enemigo.js
+++ Game/Enemigo.js
@@ -4,8 +4,8 @@ const Personaje=require('../Game/Personaje')
 
 let Enemigo=function ()
 {
-     this.mover=function(){
-        return Enemigo.prototype.
+     this.mover= function(){
+        return Enemigo.prototype
      }
 }
 Enemigo.prototype=Object.create(Personaje.prototype)
