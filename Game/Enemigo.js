589139edc5123f1627282e5d1e4c708756787daa
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2023-02-05 15:29:19 -0300

    TDD iteration

--- Game/Enemigo.js
+++ Game/Enemigo.js
@@ -4,7 +4,7 @@ const Personaje=require('../Game/Personaje')
 
 function Enemigo()
 {
-    this.prototype.saltar=function{
+    this.prototype.saltar=function(){
 
         return "salto enemigo"
     }
